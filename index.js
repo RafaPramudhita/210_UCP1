30591b8310b3d897ee1d3c2da95f7b63b61f6fad
RafaPramudhita <rafapram360@gmail.com>
2025-11-04 10:13:01 +0700

    membuat method delete

--- index.js
+++ index.js
@@ -58,3 +58,17 @@ app.put("/film/:id", async (req, res) => {
     }
 });
 
+app.delete('/film/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const film = await db.Film.findByPk(id); 
+        if (!film) {
+            return res.status(404).send({ message: 'Film tidak ditemukan' });
+        }
+
+        await film.destroy();
+        res.send({ message: 'Film berhasil dihapus' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
