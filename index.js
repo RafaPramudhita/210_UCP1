43af47dc21d44d9123fc9480fe7a1a76092436b9
RafaPramudhita <rafapram360@gmail.com>
2025-11-04 10:12:45 +0700

    membuat method put

--- index.js
+++ index.js
@@ -41,5 +41,20 @@ app.post("/film", async (req, res) => {
 });
 
 
+app.put("/film/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
 
+    try {
+        const film = await db.Film.findByPk(id);
+        if (!film) {
+            return res.status(404).send({ message: 'Film tidak ditemukan' });
+        }
+
+        await film.update(data);
+        res.send({ message: 'Film berhasil diupdate' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
