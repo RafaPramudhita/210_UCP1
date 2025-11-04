45c9831ae431e0bcfe830220d28eefc67a1cc037
RafaPramudhita <rafapram360@gmail.com>
2025-11-04 10:11:54 +0700

    membuat db sequelize

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.listen(PORT, () => {
     console.log('Server started on port 3000');
 });
 
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server started');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 
 
