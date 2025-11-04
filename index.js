59f156f72a1068aaa59f9a4a1a36cf19a7b2823f
RafaPramudhita <rafapram360@gmail.com>
2025-11-04 10:11:20 +0700

    set up node index

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(PORT, () => {
+    console.log('Server started on port 3000');
+});
+
+
+
+
+
+
+
+
+
