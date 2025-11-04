df5455c2eed534b6c8ab817c35873ce2e9f366f6
RafaPramudhita <rafapram360@gmail.com>
2025-11-04 10:10:07 +0700

    set up express.js

---
+++ models/film.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const Film = sequelize.define('Film', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+        Nama_Film: {
+            type: DataTypes.STRING,
+        },
+        Description: {
+            type: DataTypes.STRING,
+        },
+        Sutradara: {
+            type: DataTypes.STRING,
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+        },
+        genre: {
+            type: DataTypes.STRING,
+        },
+    });
+        return Film;
+};
